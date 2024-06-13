9958baf489ce6c834e4a9986ade5f50265d66d20
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-13 12:57:45 -0500

    En el modelo 'Language' creando un metodo '__str__' que devuelve el nombre del lenguaje

--- model_examples/example/models.py
+++ model_examples/example/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Language(models.Model):
     name = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.name
 
 class Framework(models.Model):
     name = models.CharField(max_length=10)
